df949959cda3c37cf4b0605331c9c82d775602a8
redthing1 <xhyinne@gmail.com>
2021-09-29 17:20:54 -0700

    basic menu elements

--- src/DusterGBA/src/menu.c
+++ src/DusterGBA/src/menu.c
@@ -3,28 +3,31 @@
 #include "tonc.h"
 #include "res.h"
 
-Sprite* menu;
+Sprite *menu;
 Anim th_anim1;
 
-void menu_start() {
+void menu_start()
+{
     dusk_init_graphics_mode0();
 
-    pal_bg_mem[0] = RES_PAL[2]; // bg col
-    
-
-    // text
-
-    REG_DISPCNT |= DCNT_BG1;
-    tte_init_chr4c(1, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_GRAY, NULL, NULL);
+    // init tte
+    REG_DISPCNT |= DCNT_BG0;
+    tte_init_chr4c(0, BG_CBB(1) | BG_SBB(31), 0, 0x0201, CLR_GRAY, NULL, NULL);
     tte_init_con();
 
-    pal_bg_mem[2] = RES_PAL[0];
+    // load bg for title
+    GritImage bg_img = dusk_load_image("title");
+    dusk_background_upload_raw(&bg_img, 0, 30);
 
-    tte_printf("#{P:12,12}#{ci:2}duster");
-    tte_printf("#{P:200,140}#{ci:2}play >");
+    dusk_background_make(1, BG_REG_32x32, (Background){.x = 0, .y = 0, .cbb = 0, .sbb = 30});
+
+    // draw text
+    pal_bg_mem[16] = RES_PAL[0];
+    tte_printf("#{P:200,140}#{ci:16}play >");
 }
 
-void menu_update() {
+void menu_update()
+{
     dusk_frame();
 
     // update sprites
