407f16043fde53a7d5ce647f96c791abb6af6eb7
redthing1 <xhyinne@gmail.com>
2021-10-02 09:16:21 -0700

    add vpos

---
+++ src/DusterGBA/src/vpos.c
@@ -0,0 +1,5 @@
+#include "vpos.h"
+
+VPos vpos_zero() { return (VPos){.x = 0, .y = 0}; }
+VPos vpos_one() { return (VPos){.x = 1, .y = 1}; }
+VPos vpos_add(VPos p1, VPos p2) { return (VPos){.x = p1.x + p2.x +, .y = p1.y + p2.y}; }
---
+++ src/DusterGBA/src/vpos.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <tonc_types.h>
+
+typedef struct {
+    u8 x;
+    u8 y;
+} VPos;
+
+typedef struct {
+    s16 x;
+    s16 y;
+} VPos16;
+
+VPos vpos_zero();
+VPos vpos_one();
+VPos vpos_add(VPos p1, VPos p2);
+// VPos vpos_neg(VPos p1);
+// VPos vpos_multi(VPos p1, int s);
+// VPos vpos_multf(VPos p1, int s);
+// VPos vpos_lerp(VPos p1, VPos p2);
