afd0e49b90002a66ad5bccc5ecca05148f830b06
redthing1 <xhyinne@gmail.com>
2021-10-02 10:13:54 -0700

    add hashtable get test

--- src/DusterGBA/src/scn/board/board_anim.c
+++ src/DusterGBA/src/scn/board/board_anim.c
@@ -16,9 +16,22 @@ void update_pawn_tween() {
     // cc_hashtable_get_keys(pawn2sprite, &pawn2sprite_keys);
     // cc_array_destroy(pawn2sprite_keys);
     // mgba_printf(MGBA_LOG_ERROR, "pawn2sprite size: %d", cc_array_size(pawn2sprite_keys));
-    
+
     cc_hashtable_foreach_key(pawn2sprite, foreach_pawn2sprite_key);
+
+    // try getting
+    int* get_test_out;
+    int get_test_key = 32;
+    enum cc_stat get_test_stat = cc_hashtable_get(pawn2sprite, &get_test_key, (void*) &get_test_out);
+    if (get_test_stat == CC_OK) {
+        mgba_printf(MGBA_LOG_ERROR, "pawn2sprite get test: %d = %d", get_test_key, *get_test_out);
+    } else {
+        mgba_printf(MGBA_LOG_ERROR, "pawn2sprite get test fail: key: %d", get_test_key);
+    }
 
     if (pawn_tween.pawn_gid < 0)
         return;
+
+    // // now we tween
+    // int pawn_sprite = cc_hashtable_get()
 }
