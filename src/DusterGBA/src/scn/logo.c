32f7b9da0196102861ff344daa9d68853df92402
redthing1 <xhyinne@gmail.com>
2021-10-03 21:08:07 -0700

    add intro sound

--- src/DusterGBA/src/scn/logo.c
+++ src/DusterGBA/src/scn/logo.c
@@ -3,14 +3,40 @@
 #include <tonc.h>
 #include "scenes.h"
 #include "res.h"
+#include "soundbank.h"
+#include "maxmod.h"
 
 Sprite* logo;
 const int FADE_LENGTH = 30; // fade length in frames
 int start_frame;
 int fade_step; // frames per fade unit
+mm_sound_effect intro_chime;
+
+void logo_init_audio() {
+    // irq setup for maxmod dma
+    irq_init(NULL);
+    irq_add(II_VBLANK, mmVBlank);
+    irq_enable(II_VBLANK);
+
+    // load soundbank
+    u32 soundbank_len;
+    const u32* soundbank_bin = gbfs_get_obj(gbfs_dat, "soundbank.bin", &soundbank_len);
+
+    // initialise maxmod with soundbank and 8 channels
+    mmInitDefault((mm_addr)soundbank_bin, 8);
+
+    // define sfx
+    intro_chime.handle = 0;
+    intro_chime.id = SFX_INTRO;
+    intro_chime.rate = (int)(1.2f * (1 << 10));
+    intro_chime.volume = 255;
+    intro_chime.panning = 128;
+}
 
 void logo_start() {
     dusk_init_graphics_mode0();
+
+    logo_init_audio();
 
     REG_DISPCNT |= DCNT_BG0;
 
@@ -43,9 +69,9 @@ void logo_start() {
 
     // ----------
 
-	REG_DISPCNT |= DCNT_BG1;
-	tte_init_chr4c(1, BG_CBB(0)|BG_SBB(31), 0, 0x0201, CLR_WHITE, NULL, NULL);
-	tte_init_con();
+    REG_DISPCNT |= DCNT_BG1;
+    tte_init_chr4c(1, BG_CBB(0) | BG_SBB(31), 0, 0x0201, CLR_WHITE, NULL, NULL);
+    tte_init_con();
 
     // pal_gradient_ex(pal_bg_mem, 1, 4, 0x6F98, 0x4964);
     pal_bg_mem[2] = RES_PAL[4];
@@ -53,10 +79,14 @@ void logo_start() {
 
     tte_printf("#{P:92,24}#{ci:2}bean machine");
     tte_printf("#{P:100,36}#{ci:3}presents");
+
+    // play sound effect
+    mmEffectEx(&intro_chime);
 }
 
 void logo_update() {
     dusk_frame();
+    mmFrame();
 
     int progress = (frame_count - start_frame);
     if (progress <= FADE_LENGTH) {
