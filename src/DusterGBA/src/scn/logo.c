a06e88328ec8646cfe4786895b2a7149814dc208
redthing1 <xhyinne@gmail.com>
2021-10-03 21:13:15 -0700

    use new intro wav

--- src/DusterGBA/src/scn/logo.c
+++ src/DusterGBA/src/scn/logo.c
@@ -28,7 +28,7 @@ void logo_init_audio() {
     // define sfx
     intro_chime.handle = 0;
     intro_chime.id = SFX_INTRO;
-    intro_chime.rate = (int)(1.2f * (1 << 10));
+    intro_chime.rate = (int)(1.0f * (1 << 10));
     intro_chime.volume = 255;
     intro_chime.panning = 128;
 }
