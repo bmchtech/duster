60857bfbaba890b047fc3f812e68f1a8744d0d78
redthing1 <xhyinne@gmail.com>
2021-10-07 09:22:11 -0700

    clean up unused log code in tween mapping

--- src/DusterGBA/src/scn/board/board_anim.c
+++ src/DusterGBA/src/scn/board/board_anim.c
@@ -1,14 +1,5 @@
 #include "board_scn.h"
 
-void foreach_pawn2sprite_key(const void* key) {
-    // try getting the value and printing
-    int* val_out;
-    cc_hashtable_get(pawn2sprite, (void*)key, (void*)&val_out);
-    int* key_out = (int*)key;
-
-    mgba_printf(MGBA_LOG_ERROR, "pawn2sprite k: %d, v: %d", *key_out, *val_out);
-}
-
 void animate_pawn_move(pawn_gid_t pawn_gid, VPos16 start_pos, VPos16 end_pos) {
     pawn_move_tween.start_pos = start_pos;
     pawn_move_tween.end_pos = end_pos;
@@ -24,25 +15,6 @@ void animate_pawn_flash(pawn_gid_t pawn_gid) {
 }
 
 void update_pawn_move_tween() {
-    // log mappings
-
-    // CC_Array* pawn2sprite_keys;
-    // cc_hashtable_get_keys(pawn2sprite, &pawn2sprite_keys);
-    // cc_array_destroy(pawn2sprite_keys);
-    // mgba_printf(MGBA_LOG_ERROR, "pawn2sprite size: %d", cc_array_size(pawn2sprite_keys));
-
-    // cc_hashtable_foreach_key(pawn2sprite, foreach_pawn2sprite_key);
-
-    // // try getting
-    // int* get_test_out;
-    // int get_test_key = 32;
-    // enum cc_stat get_test_stat = cc_hashtable_get(pawn2sprite, &get_test_key, (void*) &get_test_out);
-    // if (get_test_stat == CC_OK) {
-    //     mgba_printf(MGBA_LOG_ERROR, "pawn2sprite get test: %d = %d", get_test_key, *get_test_out);
-    // } else {
-    //     mgba_printf(MGBA_LOG_ERROR, "pawn2sprite get test fail: key: %d", get_test_key);
-    // }
-
     if (pawn_move_tween.pawn_gid < 0)
         return;
 
