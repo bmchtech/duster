8afd4a6980e9232e3cb3fdbfbbf89b5707443e3d
redthing1 <xhyinne@gmail.com>
2021-10-09 12:30:42 -0700

    pawn now flashes solid

--- src/DusterGBA/src/scn/board/board_anim.c
+++ src/DusterGBA/src/scn/board/board_anim.c
@@ -93,7 +93,7 @@ void update_pawn_flash_tween() {
         // done
 
         // TODO: code to clean up after tweeN
-        // REG_DISPCNT &= ~DCNT_WIN0;
+        REG_DISPCNT &= ~DCNT_WIN0;
         REG_BLDY = BLDY_BUILD(0);
 
         // clear tween info
@@ -114,15 +114,19 @@ void update_pawn_flash_tween() {
         if (pawn_sprite_ix < 0)
             return; // FAIL
 
-        // REG_DISPCNT |= DCNT_WIN0;
+        REG_DISPCNT |= DCNT_WIN0;
         Sprite* pawn_sprite = &sprites[pawn_sprite_ix];
 
-        // REG_WIN0H = WIN_BUILD(pawn_sprite->x, pawn_sprite->x + 8);
-        // REG_WIN0V = WIN_BUILD(pawn_sprite->y, pawn_sprite->y + 8);
-        // REG_WININ = WININ_BUILD(WIN_OBJ | WIN_BLD, 0);
-        // REG_WINOUT = WINOUT_BUILD(WIN_BG0 | WIN_BG1, 0);
+        mgba_printf(MGBA_LOG_ERROR, "window dims: %d %d %d %d", pawn_sprite->x, pawn_sprite->y, pawn_sprite->x + 8, pawn_sprite->y + 8);
+
+        REG_WIN0H = WIN_BUILD(pawn_sprite->x + 8, pawn_sprite->x);
+        REG_WIN0V = WIN_BUILD(pawn_sprite->y + 8, pawn_sprite->y);
+        REG_WININ = WININ_BUILD(WIN_OBJ | WIN_BLD, 0);
+        REG_WINOUT = WINOUT_BUILD(WIN_ALL, 0);
+
         REG_BLDCNT = BLD_OBJ | BLD_WHITE;
         REG_BLDY = BLDY_BUILD(16);
+
         mgba_printf(MGBA_LOG_ERROR, "lechuga");
     }
 
