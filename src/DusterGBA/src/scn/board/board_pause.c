caf4c359f4e5e1912763d2b0fa2a83efbaa95262
Zayd <zayd@qumsieh.net>
2021-10-10 17:55:21 -0700

    pause menu cursor moves now

--- src/DusterGBA/src/scn/board/board_pause.c
+++ src/DusterGBA/src/scn/board/board_pause.c
@@ -1,7 +1,24 @@
 #include "board_scn.h"
 
+
+#define NUM_PAUSE_SELECTIONS 3
+VPos pause_cursor_pos;
+int pause_cursor_selection = 0;
+
 void update_pause_ui() {
-    
+    if (key_hit(KEY_DOWN) && pause_cursor_selection < (NUM_PAUSE_SELECTIONS - 1)) {
+        pause_cursor_selection++;
+        pausemenu_dirty = TRUE;
+    }
+
+    if (key_hit(KEY_UP) && pause_cursor_selection > 0) {
+        pause_cursor_selection--;
+        pausemenu_dirty = TRUE;
+    }
+}
+
+int get_pause_cursor_y() {
+    return 22 + pause_cursor_selection * 12 + 6;
 }
 
 void draw_pause_ui() {
@@ -27,7 +44,7 @@ void draw_pause_ui() {
     // box
 
     // for now just draw something
-    schr4r_hline(&bg0_srf, 80, 40, 85, 1);
+    schr4c_hline(&bg0_srf, 50, get_pause_cursor_y(), 55, 1);
 
     // todo
 }
--- src/DusterGBA/src/scn/board/board_scn.h
+++ src/DusterGBA/src/scn/board/board_scn.h
@@ -61,6 +61,7 @@ extern BOOL request_step;
 extern BOOL pawn_move_range_dirty;
 extern BoardScenePage board_scene_page;
 extern BOOL pausemenu_dirty;
+extern int pause_cursor_selection;
 
 void set_ui_dirty();
 
