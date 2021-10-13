75cc91375dee40b32d6f285f4b452af26304690e
redthing1 <xhyinne@gmail.com>
2021-10-12 22:00:27 -0700

    add emulatorjs

---
+++ locaterom.js
@@ -0,0 +1,5 @@
+var rom_name = 'Duster.gba';
+var release_tag = 'v0.1';
+var releases_url = 'https://github.com/redthing1/duster/releases/download/';
+
+var DUSTER_ROM_LOC = `${releases_url}/${release_tag}/${rom_name}`;
