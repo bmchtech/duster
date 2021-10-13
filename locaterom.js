e7d3b35b0d51cf12089b526ad2211e626e148218
redthing1 <xhyinne@gmail.com>
2021-10-12 22:20:49 -0700

    add dl and upl buttons

--- locaterom.js
+++ locaterom.js
@@ -3,3 +3,21 @@ var release_tag = 'v0.1';
 var releases_url = 'https://github.com/redthing1/duster/releases/download/';
 
 var DUSTER_ROM_LOC = `${releases_url}/${release_tag}/${rom_name}`;
+
+var LOADED_GAME_ROM = null;
+
+function on_get_rom_click() {
+    window.open(DUSTER_ROM_LOC);
+}
+
+window.addEventListener('load', function () {
+    document.getElementById('file').onchange = function (e) {
+        var gameUrl = URL.createObjectURL(new Blob([e.target.files[0]]))
+        EJS_gameUrl = gameUrl;
+        console.log('new file ready: ', EJS_gameUrl);
+
+        var script = document.createElement('script');
+        script.src = 'data/loader.js';
+        document.body.appendChild(script);
+    }
+})
