5641a7a42dc829f4878564abba916b8ecf6b33f2
redthing1 <xhyinne@gmail.com>
2021-11-02 15:35:55 -0700

    working nelua interop

--- src/DusterGBA/src/lua/bap.lua
+++ src/DusterGBA/src/lua/bap.lua
@@ -1,5 +1,17 @@
+require 'vector'
+
 -- `cexport` marks this function to be exported
 -- `codename` fix the generated C code name
-local function bap_nelua_add(x: int32, y: int32): int32 <cexport, codename 'bap_nelua_add'>
-    return 1
+local function bap_nelua_add(a: int32, b: int32): int32 <cexport, codename 'bap_nelua_add'>
+    local vec: vector(int32) = {}
+
+    vec:push(a)
+    vec:push(b)
+
+    local t2 = vec:pop()
+    local t1 = vec:pop()
+
+    vec:destroy()
+
+    return t1 + t2
 end
