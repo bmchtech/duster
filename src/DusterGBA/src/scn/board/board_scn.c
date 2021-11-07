e89cc7241cd777aebaf7a361bca72856c8558474
redthing1 <xhyinne@gmail.com>
2021-11-06 23:27:01 -0700

    fix initial movequeue to empty

--- src/DusterGBA/src/scn/board/board_scn.c
+++ src/DusterGBA/src/scn/board/board_scn.c
@@ -26,9 +26,9 @@ BOOL pausemenu_dirty = TRUE;
 int board_scroll_x = 0;
 int board_scroll_y = 0;
 int sidebar_page = 0;
-int movequeue_length = 0;
+int movequeue_length = -1;
 QueuedMove movequeue_queue[MOVEQUEUE_MAX_SIZE];
-int movequeue_progress = 0;
+int movequeue_progress = -1;
 int movequeue_delay_timer = 0;
 int ai_played_move = -1;
 
