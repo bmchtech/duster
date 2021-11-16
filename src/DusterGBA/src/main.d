796e6cd4929302ac34928a6b8a2292b5d8a56c8c
redthing1 <xhyinne@gmail.com>
2021-11-15 20:11:17 -0800

    working hello world

--- src/DusterGBA/src/main.d
+++ src/DusterGBA/src/main.d
@@ -1,8 +1,13 @@
 import core.stdc.stdio;
+import mgba;
 
 extern(C) int main() {
 	// nothing
 	printf("Hello, world!\n");
+
+	if (mgba_open()) {
+		mgba_printf(MGBA_LOG_LEVEL.MGBA_LOG_ERROR, "Hello, world, %s!\n", cast(const char*)"Bean Machine");
+	}
 
 	return 0;
 }
