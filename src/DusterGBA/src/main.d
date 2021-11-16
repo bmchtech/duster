1002c484a2ec231dc66e6500d9e72cabfc2c8f97
redthing1 <xhyinne@gmail.com>
2021-11-16 00:23:42 -0800

    reeference dusk stubs

--- src/DusterGBA/src/main.d
+++ src/DusterGBA/src/main.d
@@ -1,6 +1,8 @@
 import core.stdc.stdio;
-import mgba;
 import tonc;
+import dusk.contrib.mgba;
+import logo;
+import dusk;
 
 extern(C) int main() {
 	// nothing
@@ -9,6 +11,16 @@ extern(C) int main() {
 	if (mgba_open()) {
 		mgba_printf(MGBA_LOG_LEVEL.MGBA_LOG_ERROR, "Hello, world, %s!\n", cast(const char*)"Bean Machine");
 	}
+
+	// start logo scene
+	dusk_init_all();
+
+    dusk_scene_set(logo_scene);
+
+    while (TRUE) {
+        key_poll(); // update inputF
+        dusk_scene_update();
+    }
 
 	return 0;
 }
