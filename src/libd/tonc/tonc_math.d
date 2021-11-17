c643f38bb22e84f37f631b0737a3b2fa98f9c24a
redthing1 <xhyinne@gmail.com>
2021-11-16 17:39:59 -0800

    add inline funcs to tonc math

--- src/libd/tonc/tonc_math.d
+++ src/libd/tonc/tonc_math.d
@@ -49,7 +49,7 @@ extern (C):
 /*!	\addtogroup grpMathBase	*/
 /*! \{	*/
 
-// Also available as inline functions
+// Also available as functions
 
 //! \name core math macros
 //\{
@@ -239,8 +239,8 @@ int lu_sin (uint theta);
 int lu_cos (uint theta);
 uint lu_div (uint x);
 
-int lu_lerp32 (const(int)* lut, uint x, const uint shift);
-int lu_lerp16 (const(short)* lut, uint x, const uint shift);
+// int lu_lerp32 (const(int)* lut, uint x, const uint shift);
+// int lu_lerp16 (const(short)* lut, uint x, const uint shift);
 
 /*! \}	*/
 
@@ -334,81 +334,110 @@ VECTOR* vec_cross (VECTOR* vd, const(VECTOR)* va, const(VECTOR)* vb);
 
 /*!	\}	*/
 
-// === INLINE =========================================================
+// === =========================================================
 
 // --- General --------------------------------------------------------
 
 //! Get the sign of \a x.
-int sgn (int x);
+int sgn(int x)
+{	return (x>=0) ? +1 : -1;				}
 
 //! Tri-state sign of \a x: -1 for negative, 0 for 0, +1 for positive.
-int sgn3 (int x);
+int sgn3(int x)
+{	return (x>>31) - (-x>>31);				}
 
 //! Get the maximum of \a a and \a b
-int max (int a, int b);
+int max(int a, int b)
+{	return (a > b) ? (a) : (b);				}
 
 //! Get the minimum of \a a and \a b
-int min (int a, int b);
+int min(int a, int b)
+{	return (a < b) ? (a) : (b);				}
+
 
 //! Range check
-BOOL in_range (int x, int min, int max);
+BOOL in_range(int x, int min, int max)
+{	return cast(u32)(x-min) < cast(u32)(max-min);	}
+
 
 //! Truncates \a x to stay in range [\a min, \a max>
 /*!	\return Truncated value of \a x.
 *	\note	\a max is exclusive!
 */
-int clamp (int x, int min, int max);
+int clamp(int x, int min, int max)
+{	return (x>=max) ? (max-1) : ( (x<min) ? min : x );	}
 
 //! Reflects \a x at boundaries \a min and \a max
-/*! If \a x is outside the range [\a min, \a max>,
-*	  it'll be placed inside again with the same distance
-*	  to the 'wall', but on the other side. Example for lower
+/*! If \a x is outside the range [\a min, \a max>, 
+*	  it'll be placed inside again with the same distance 
+*	  to the 'wall', but on the other side. Example for lower 
 *	  border: y = \a min - (\a x- \a min)  = 2*\a min + \a x.
 *	\return	Reflected value of \a x.
 *	\note	\a max is exclusive!
 */
-int reflect (int x, int min, int max);
+int reflect(int x, int min, int max)
+{	return (x>=max) ? (2*(max-1)-x)	: ( (x<min) ? (2*min-x)	: x );		}
 
 //! Wraps \a x to stay in range [\a min, \a max>
-int wrap (int x, int min, int max);
+int wrap(int x, int min, int max)
+{	return (x>=max) ? (x+min-max)	: ( (x<min) ? (x+max-min) : x );	}
+
 
 // --- Fixed point ----------------------------------------------------
 
+
 //! Convert an integer to fixed-point
-FIXED int2fx (int d);
+FIXED int2fx(int d)
+{	return d<<FIX_SHIFT;	}
 
 //! Convert a float to fixed-point
-FIXED float2fx (float f);
+FIXED float2fx(float f)
+{	return cast(FIXED)(f*FIX_SCALEF);	}
+
 
 //! Convert a FIXED point value to an unsigned integer (orly?).
-uint fx2uint (FIXED fx);
+u32 fx2uint(FIXED fx)	
+{	return fx>>FIX_SHIFT;	}
 
 //! Get the unsigned fractional part of a fixed point value (orly?).
-uint fx2ufrac (FIXED fx);
+u32 fx2ufrac(FIXED fx)
+{	return fx&FIX_MASK;	}
 
 //! Convert a FIXED point value to an signed integer.
-int fx2int (FIXED fx);
+int fx2int(FIXED fx)
+{	return fx/FIX_SCALE;	}
 
 //! Convert a fixed point value to floating point.
-float fx2float (FIXED fx);
+float fx2float(FIXED fx)
+{	return fx/FIX_SCALEF;	}
 
 //! Add two fixed point values
-FIXED fxadd (FIXED fa, FIXED fb);
+FIXED fxadd(FIXED fa, FIXED fb)
+{	return fa + fb;			}
 
 //! Subtract two fixed point values
-FIXED fxsub (FIXED fa, FIXED fb);
+FIXED fxsub(FIXED fa, FIXED fb)
+{	return fa - fb;			}
+
 
 //! Multiply two fixed point values
-FIXED fxmul (FIXED fa, FIXED fb);
+FIXED fxmul(FIXED fa, FIXED fb)
+{	return (fa*fb)>>FIX_SHIFT;				}
 
 //! Divide two fixed point values.
-FIXED fxdiv (FIXED fa, FIXED fb);
+FIXED fxdiv(FIXED fa, FIXED fb)
+{	return ((fa)*FIX_SCALE)/(fb);			}
+
 
 //! Multiply two fixed point values using 64bit math.
-FIXED fxmul64 (FIXED fa, FIXED fb);
+FIXED fxmul64(FIXED fa, FIXED fb)
+{	return cast(FIXED) ((cast(s64)fa)*fb)>>FIX_SHIFT;		}
+
 
 //! Divide two fixed point values using 64bit math.
-FIXED fxdiv64 (FIXED fa, FIXED fb);
+FIXED fxdiv64(FIXED fa, FIXED fb)
+{	return ( (cast(s64)fa)<<FIX_SHIFT)/(fb);	}
+
 
 // --- LUT ------------------------------------------------------------
 
@@ -416,65 +445,121 @@ FIXED fxdiv64 (FIXED fa, FIXED fb);
 /*! \param theta Angle in [0,FFFFh] range
 *	 \return .12f sine value
 */
-int lu_sin (uint theta);
+s32 lu_sin(uint theta)
+{	return sin_lut[(theta>>7)&0x1FF];	}
 
 //! Look-up a cosine value (2&#960; = 0x10000)
 /*! \param theta Angle in [0,FFFFh] range
 *	 \return .12f cosine value
 */
-int lu_cos (uint theta);
+s32 lu_cos(uint theta)
+{	return sin_lut[((theta>>7)+128)&0x1FF];	}
 
 //! Look-up a division value between 0 and 255
 /*! \param x reciprocal to look up.
 *	 \return 1/x (.16f)
 */
-uint lu_div (uint x);
+uint lu_div(uint x)
+{	return div_lut[x];	}
+
 
 //! Linear interpolator for 32bit LUTs.
 /*! A lut is essentially the discrete form of a function, f(<i>x</i>).
-*	You can get values for non-integer \e x via (linear)
+*	You can get values for non-integer \e x via (linear) 
 *	interpolation between f(x) and f(x+1).
 *	\param lut	The LUT to interpolate from.
 *	\param x	Fixed point number to interpolate at.
 *	\param shift	Number of fixed-point bits of \a x.
 */
-int lu_lerp32 (const(int)* lut, uint x, const uint shift);
+int lu_lerp32(const s32[] lut, uint x, const uint shift)
+{
+	int xa, ya, yb;
+	xa=x>>shift;
+	ya= lut[xa]; yb= lut[xa+1];
+	return ya + ( (yb-ya)*(x-(xa<<shift))>>shift );
+}
 
 //! As lu_lerp32, but for 16bit LUTs.
-int lu_lerp16 (const(short)* lut, uint x, const uint shift);
+int lu_lerp16(const s16[] lut, uint x, const uint shift)
+{
+	int xa, ya, yb;
+	xa=x>>shift;
+	ya= lut[xa]; yb= lut[xa+1];
+	return ya + ( (yb-ya)*(x-(xa<<shift))>>shift );	
+}
+
 
 // --- Point ----------------------------------------------------------
 
 //! Initialize \a pd to (\a x, \a y)
-POINT* pt_set (POINT* pd, int x, int y);
+POINT *pt_set(POINT *pd, int x, int y)
+{
+	pd.x= x;	pd.y= y;
+	return pd;
+}
 
 //! Point addition: \a pd = \a pa + \a pb
-POINT* pt_add (POINT* pd, const(POINT)* pa, const(POINT)* pb);
+POINT *pt_add(POINT *pd, const POINT *pa, const POINT *pb)
+{
+	pd.x= pa.x + pb.x;	
+	pd.y= pa.x + pb.y;
+	return pd;
+}
 
 //! Point subtraction: \a pd = \a pa - \a pb
-POINT* pt_sub (POINT* pd, const(POINT)* pa, const(POINT)* pb);
+POINT *pt_sub(POINT *pd, const POINT *pa, const POINT *pb)
+{
+	pd.x= pa.x - pb.x;	
+	pd.y= pa.x - pb.y;
+	return pd;
+}
 
 //! Point scale: \a pd = \a c * \a pa 
-POINT* pt_scale (POINT* pd, const(POINT)* pa, int c);
+POINT *pt_scale(POINT *pd, const POINT *pa, int c)
+{	
+	pd.x= pa.x*c;	
+	pd.y= pa.y*c;	
+	return pd;
+}
 
 //! Point  increment: \a pd += \a pb
-POINT* pt_add_eq (POINT* pd, const(POINT)* pb);
+POINT *pt_add_eq(POINT *pd, const POINT *pb)
+{	
+	pd.x += pb.y;	
+	pd.y += pb.y;	
+	return pd;	
+}
 
 //! Point decrement: \a pd -= \a pb
-POINT* pt_sub_eq (POINT* pd, const(POINT)* pb);
+POINT *pt_sub_eq(POINT *pd, const POINT *pb)
+{	
+	pd.x -= pb.y;	
+	pd.y -= pb.y;	
+	return pd;	
+}
 
 //! Point scale: \a pd *= \a c
-POINT* pt_scale_eq (POINT* pd, int c);
+POINT *pt_scale_eq(POINT *pd, int c)
+{	
+	pd.x *= c;		
+	pd.y *= c;		
+	return pd;	
+}
 
 //! Point 'cross'-product: \a pa \htmlonly &times; \endhtmlonly \a pb
-/*! Actually, there's no such thing as a 2D cross-product, but you could
-*	  extend it to 3D and get the value of its <i>z</i>-component,
+/*! Actually, there's no such thing as a 2D cross-product, but you could 
+*	  extend it to 3D and get the value of its <i>z</i>-component, 
 *	  which can be used for a test for parallelism.
 */
-int pt_cross (const(POINT)* pa, const(POINT)* pb);
+int pt_cross(const POINT *pa, const POINT *pb)
+{	return pa.x * pb.y - pa.y * pb.x;			}
+
 
 //! Point 'dot'-product:\a pa \htmlonly &middot; \endhtmlonly \a pb
-int pt_dot (const(POINT)* pa, const(POINT)* pb);
+int pt_dot(const POINT *pa, const POINT *pb)
+{	return pa.x * pb.x + pa.y * pb.y;			}
+
+
 
 // --- Rect -----------------------------------------------------------
 
@@ -484,7 +569,11 @@ int pt_dot (const(POINT)* pa, const(POINT)* pb);
 *	\param r	Right side.
 *	\param b	Bottom side.
 */
-RECT* rc_set (RECT* rc, int l, int t, int r, int b);
+RECT *rc_set(RECT *rc, int l, int t, int r, int b)
+{
+	rc.left= l; rc.top= t; rc.right= r; rc.bottom= b;
+	return rc;
+}
 
 //! Initialize a rectangle, with sizes inside of max boundaries.
 /*!	\param x	Left side.
@@ -492,59 +581,139 @@ RECT* rc_set (RECT* rc, int l, int t, int r, int b);
 *	\param w	Width.
 *	\param h	Height.
 */
-RECT* rc_set2 (RECT* rc, int x, int y, int w, int h);
+RECT *rc_set2(RECT *rc, int x, int y, int w, int h)
+{
+	rc.left= x; rc.top= y; rc.right= x+w; rc.bottom= y+h;
+	return rc;
+}
 
 //! Get rectangle width.
-int rc_width (const(RECT)* rc);
+int rc_width(const RECT *rc)
+{	return rc.right - rc.left;	}
 
 //! Get rectangle height
-int rc_height (const(RECT)* rc);
+int rc_height(const RECT *rc)
+{	return rc.bottom - rc.top;	}
 
 //! Move rectangle to (\a x, \a y) position.
-RECT* rc_set_pos (RECT* rc, int x, int y);
+RECT *rc_set_pos(RECT *rc, int x, int y)
+{
+	rc.right += x-rc.left;	rc.left= x;
+	rc.bottom += y-rc.top;	rc.top= y;
+	return rc;
+}
 
 //! Reside rectangle.
-RECT* rc_set_size (RECT* rc, int w, int h);
+RECT *rc_set_size(RECT *rc, int w, int h)
+{
+	rc.right= rc.left+w;		rc.bottom= rc.top+h;
+	return rc;
+}
 
 //! Move rectangle by (\a dx, \a dy).
-RECT* rc_move (RECT* rc, int dx, int dy);
+RECT *rc_move(RECT *rc, int dx, int dy)
+{
+	rc.left += dx;		rc.top += dy;
+	rc.right += dx;	rc.bottom += dy;
+	return rc;
+}
 
 //! Increase size by \a dw horizontally and \a dh vertically.
-RECT* rc_inflate (RECT* rc, int dw, int dh);
+RECT *rc_inflate(RECT *rc, int dw, int dh)
+{	
+	rc.left -= dw;		rc.top -= dh;	
+	rc.right += dw;	rc.bottom += dh;
+	return rc;
+}
 
 //! Increase sizes on all sides by values of rectangle \a dr.
-RECT* rc_inflate2 (RECT* rc, const(RECT)* dr);
+RECT *rc_inflate2(RECT *rc, const RECT *dr)
+{	
+	rc.left += dr.left;	rc.top += dr.top;	
+	rc.right += dr.right;	rc.bottom += dr.bottom;
+	return rc;
+}
+
 
 // --- Vector ---------------------------------------------------------
 
 //! Initialize a vector
-VECTOR* vec_set (VECTOR* vd, FIXED x, FIXED y, FIXED z);
+VECTOR *vec_set(VECTOR *vd, FIXED x, FIXED y, FIXED z)
+{
+	vd.x= x; vd.y= y; vd.z= z;
+	return vd;
+}
 
 //! Add vectors: \b d = \b a + \b b;
-VECTOR* vec_add (VECTOR* vd, const(VECTOR)* va, const(VECTOR)* vb);
+VECTOR *vec_add(VECTOR *vd, const VECTOR *va, const VECTOR *vb)
+{
+	vd.x= va.x + vb.x;
+	vd.y= va.y + vb.y;
+	vd.z= va.z + vb.z;
+	return vd;
+}
 
 //! Subtract vectors: \b d = \b a - \b b;
-VECTOR* vec_sub (VECTOR* vd, const(VECTOR)* va, const(VECTOR)* vb);
+VECTOR *vec_sub(VECTOR *vd, const VECTOR *va, const VECTOR *vb)
+{
+	vd.x= va.x - vb.x;
+	vd.y= va.y - vb.y;
+	vd.z= va.z - vb.z;
+	return vd;
+}
 
-//! Multiply vectors elements: \b d = \b S(ax, ay, az) �\b b
-VECTOR* vec_mul (VECTOR* vd, const(VECTOR)* va, const(VECTOR)* vb);
+//! Multiply vectors elements: \b d = \b S(ax, ay, az) ·\b b
+VECTOR *vec_mul(VECTOR *vd, const VECTOR *va, const VECTOR *vb)
+{
+	vd.x= fxmul(va.x, vb.x);
+	vd.y= fxmul(va.y, vb.y);
+	vd.z= fxmul(va.z, vb.z);
+	return vd;
+}
 
 //! Scale vector: \b d = c*\b a
-VECTOR* vec_scale (VECTOR* vd, const(VECTOR)* va, FIXED c);
+VECTOR *vec_scale(VECTOR *vd, const VECTOR *va, FIXED c)
+{
+	vd.x= fxmul(va.x, c);
+	vd.y= fxmul(va.y, c);
+	vd.z= fxmul(va.z, c);
+	return vd;
+}
 
-//! Dot-product: d = \b a �\b b
-FIXED vec_dot (const(VECTOR)* va, const(VECTOR)* vb);
+//! Dot-product: d = \b a ·\b b
+FIXED vec_dot(const VECTOR *va, const VECTOR *vb)
+{	
+	FIXED dot;
+	dot  = fxmul(va.x, vb.x);
+	dot += fxmul(va.y, vb.y);
+	dot += fxmul(va.z, vb.z);
+	return dot;
+}
 
 //! Increment vector: \b d += \b b;
-VECTOR* vec_add_eq (VECTOR* vd, const(VECTOR)* vb);
+VECTOR *vec_add_eq(VECTOR *vd, const VECTOR *vb)
+{	vd.x += vb.x;	vd.y += vb.y;	vd.z += vb.z;	return vd;	}
 
 //! Decrease vector: \b d -= \b b;
-VECTOR* vec_sub_eq (VECTOR* vd, const(VECTOR)* vb);
+VECTOR *vec_sub_eq(VECTOR *vd, const VECTOR *vb)
+{	vd.x -= vb.x;	vd.y -= vb.y;	vd.z -= vb.z;	return vd;	}
 
-//! Multiply vectors elements: \b d = \b S(dx, dy, dz) �\b b
-VECTOR* vec_mul_eq (VECTOR* vd, const(VECTOR)* vb);
+//! Multiply vectors elements: \b d = \b S(dx, dy, dz) ·\b b
+VECTOR *vec_mul_eq(VECTOR *vd, const VECTOR *vb)
+{
+	vd.x= fxmul(vd.x, vb.x);
+	vd.y= fxmul(vd.y, vb.y);
+	vd.z= fxmul(vd.z, vb.z);
+	return vd;
+}
 
 //! Scale vector: \b d = c*\b d
-VECTOR* vec_scale_eq (VECTOR* vd, FIXED c);
+VECTOR *vec_scale_eq(VECTOR *vd, FIXED c)
+{
+	vd.x= fxmul(vd.x, c);
+	vd.y= fxmul(vd.y, c);
+	vd.z= fxmul(vd.z, c);
+	return vd;
+}
 
 // TONC_MATH
