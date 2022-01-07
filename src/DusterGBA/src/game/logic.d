de596a91cb9da10186f319d6d92805a461baed1b
redthing1 <xhyinne@gmail.com>
2022-01-07 00:07:29 -0800

    port logic.d

--- src/DusterGBA/src/game/logic.d
+++ src/DusterGBA/src/game/logic.d
@@ -2,6 +2,7 @@ module game.logic;
 
 import ldc.attributes;
 
+import dusk.contrib.mgba;
 import tonc;
 import game;
 
@@ -10,7 +11,7 @@ extern (C):
 void game_logic_step() {
     game_state.steps++; // counter
 
-    mgba_printf(ERROR, "logic step (%d)", game_state.steps);
+    mgba_printf(MGBALogLevel.ERROR, "logic step (%d)", game_state.steps);
 
     int turn_team = game_util_whose_turn();
     // check if all pawns on the current team have moved
@@ -49,7 +50,7 @@ void game_logic_kill_if_dead(pawn_gid_t pawn_gid) {
         pawn.alive = false;
         int pawn_tile = board_find_pawn_tile(pawn_gid);
         board_set_pawn(pawn_tile, -1);
-        mgba_printf(ERROR, "pawn died: %d", pawn_gid);
+        mgba_printf(MGBALogLevel.ERROR, "pawn died: %d", pawn_gid);
     }
 }
 
@@ -97,7 +98,7 @@ void game_logic_interact(pawn_gid_t initiator, pawn_gid_t receiver) {
         HostileUnitDuel duel = game_logic_calc_hostile_damage(initiator_pawn, receiver_pawn);
 
         // exchange damage
-        mgba_printf(ERROR, "damage exchanged: %d %d", duel.main_dmg, duel.counter_dmg);
+        mgba_printf(MGBALogLevel.ERROR, "damage exchanged: %d %d", duel.main_dmg, duel.counter_dmg);
         receiver_pawn.unit_data.hitpoints -= duel.main_dmg;
         initiator_pawn.unit_data.hitpoints -= duel.counter_dmg;
 
