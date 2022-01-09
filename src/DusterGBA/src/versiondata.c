1a2c24a8523b67b607db7ffa1a367aab020a1480
redthing1 <xhyinne@gmail.com>
2022-01-08 21:01:10 -0800

    bump version

--- src/DusterGBA/src/versiondata.c
+++ src/DusterGBA/src/versiondata.c
@@ -1,4 +1,4 @@
-#define RES_GAME_VERSION "$DUSTER v0.3.0-dport"
+#define RES_GAME_VERSION "$DUSTER v0.3.1"
 #define RES_GAME_COPYING "$COPYING Copyright (C) 2018-2022 bean machine."
 #ifndef GAME_BUILD_ID
 #define GAME_BUILD_ID "unknown"
