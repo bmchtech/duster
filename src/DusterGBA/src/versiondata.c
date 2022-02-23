d93524e09a58af318b75d652c29dd190706a5045
redthing1 <redthing1@alt.icu>
2022-02-22 20:07:01 -0800

    improve versiondata and makefile for version

--- src/DusterGBA/src/versiondata.c
+++ src/DusterGBA/src/versiondata.c
@@ -4,18 +4,16 @@
  * 
  *****************************************************************************/
 
-#define D_VERSION_ID "v0.4.2"
-
-#define RES_GAME_VERSION ("$DUSTER " D_VERSION_ID)
+#define RES_GAME_VERSION ("$DUSTER " VERSION_ID_DEF)
 #define RES_GAME_COPYING "$COPYING Copyright (C) 2018-2022 bean machine."
 
-#ifndef GAME_BUILD_ID
-#define GAME_BUILD_ID "unknown"
+#ifndef GAME_BUILD_ID_DEF
+#define GAME_BUILD_ID_DEF "unknown"
 #endif
-#define RES_GAME_BUILD ("$BUILD " GAME_BUILD_ID)
+#define RES_GAME_BUILD ("$BUILD " GAME_BUILD_ID_DEF)
 
-__attribute__((used)) const char* VERSION_ID = D_VERSION_ID;
-__attribute__((used)) const char* BUILD_ID = GAME_BUILD_ID;
+__attribute__((used)) const char* VERSION_ID = VERSION_ID_DEF;
+__attribute__((used)) const char* BUILD_ID = GAME_BUILD_ID_DEF;
 
 __attribute__((used)) const char* GAME_VERSION = RES_GAME_VERSION;
 __attribute__((used)) const char* GAME_BUILD = RES_GAME_BUILD;
